3b6d4f8ea1735f4678aa99a90f9648438863876e
Manoj Prabhakaran J <manojprabhakaranjai2012@gmail.com>
2025-08-22 23:28:21 +0530

    Create 367. Valid Perfect Square.java

---
+++ 367. Valid Perfect Square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==0 || num==1)return true;
+
+        long start = 1,end = num;
+        while(start<end){
+            long mid = start+(end-start)/2;
+            long sqr = mid*mid;
+
+            if(sqr==num){
+                return true;
+            }
+            else if(sqr<num){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+}
